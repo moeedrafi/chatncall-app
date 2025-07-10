91a5ef811134179afde5c800247cf3d9231775ed
Moeed Rafi <moeedrafi95@gmail.com>
2025-07-10 11:28:20 +0500

    feat: Message Routes

---
+++ backend/src/routes/message.routes.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/message.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.route("/").get(verifyJWT, registerUser);
+router.route("/").post(verifyJWT, loginUser);
+router.route("/").delete(verifyJWT, logoutUser);
+
+export default router;
