e3f138dd097e056a99fdd306efd86564a1d1e78e
Moeed Rafi <moeedrafi95@gmail.com>
2025-07-10 09:44:33 +0500

    feat: User Controller

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+import { User } from "../models/user.models.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {});
+
+const loginUser = asyncHandler(async (req, res) => {});
+
+const logoutUser = asyncHandler(async (req, res) => {});
+
+const refreshAccessToken = asyncHandler(async (req, res) => {});
+
+const checkAuth = asyncHandler(async (req, res) => {});
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, checkAuth };
