8146478f13771540106c0eea781cfb243d2ac7bd
Moeed Rafi <moeedrafi95@gmail.com>
2025-07-10 10:11:00 +0500

    feat: Friend Request Model

---
+++ backend/src/models/friendRequest.models.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const friendRequestSchema = new Schema(
+  {
+    status: {
+      type: String,
+      enum: ["pending", "accepted", "rejected"],
+      default: "pending",
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const FriendRequest = mongoose.model(
+  "FriendRequest",
+  friendRequestSchema
+);
