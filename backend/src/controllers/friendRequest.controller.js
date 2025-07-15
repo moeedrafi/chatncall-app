466bbdb0b875a50dd7d015ebca950f1b4be16adf
Moeed Rafi <moeedrafi95@gmail.com>
2025-07-15 10:10:33 +0500

    feat: Get Requests List Controller

--- backend/src/controllers/friendRequest.controller.js
+++ backend/src/controllers/friendRequest.controller.js
@@ -3,7 +3,18 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { FriendRequest } from "../models/friendRequest.models.js";
 
-const getRequests = asyncHandler(async (req, res) => {});
+const getRequests = asyncHandler(async (req, res) => {
+  const receivedRequests = await FriendRequest.find({
+    receiver: req.user._id,
+    status: "pending",
+  })
+    .populate("sender", "username email avatar")
+    .sort({ createdAt: -1 });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, receivedRequests, "Friend Requests Received!"));
+});
 
 const getFriendsList = asyncHandler(async (req, res) => {});
 
