f39d23c903b9741d0770d77b83c12eef2d03f63c
Moeed Rafi <moeedrafi95@gmail.com>
2025-07-16 09:56:28 +0500

    feat: Post Messages Controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -4,7 +4,31 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { Conversation } from "../models/conversation.models.js";
 
-const newMessage = asyncHandler(async (req, res) => {});
+const newMessage = asyncHandler(async (req, res) => {
+  const { body } = req.body;
+  const { conversationId } = req.params;
+  if (!body || !conversationId) {
+    throw new ApiError(400, "Missing fields");
+  }
+
+  const existingConversation = await Conversation.findById(conversationId);
+  if (
+    !existingConversation ||
+    !existingConversation.users.includes(req.user._id)
+  ) {
+    throw new ApiError(404, "Conversation not found!");
+  }
+
+  const message = await Message.create({
+    body,
+    sender: req.user._id,
+    conversation: conversationId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, message, "Fetched Messages!"));
+});
 
 const getConversationMessages = asyncHandler(async (req, res) => {
   const { conversationId } = req.params;
--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -9,9 +9,9 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.route("/").post(verifyJWT, newMessage);
 router.route("/:id").delete(verifyJWT, deleteMessage);
 router.route("/:id/seen").post(verifyJWT, seenMessage);
+router.route("/:conversationId").post(verifyJWT, newMessage);
 router.route("/:conversationId").get(verifyJWT, getConversationMessages);
 
 export default router;
